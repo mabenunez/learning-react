01c541bca9ed777542932c77bf48e0a27300f862
mabenunez <39781996+mabenunez@users.noreply.github.com>
2019-07-31 12:26:22 -0300

    added functions that set states onMouseOver and onMouseOut

--- simple-events/src/App.js
+++ simple-events/src/App.js
@@ -6,30 +6,50 @@ class App extends Component {
     super();
     this.state = {
       btnText : "Click me",
-      clicked : false
+      clicked : false,
+      otherDivClass : "hidden"
     }
-    this.changeBtnMessage = this.changeBtnMessage.bind(this)
+    this.changeBtnMessage = this.changeBtnMessage.bind(this);
+    this.appearSideDiv = this.appearSideDiv.bind(this);
+    this.dissapearDiv = this.dissapearDiv.bind(this);
   }
 
   changeBtnMessage(){
-    this.setState ({
-      clicked : true,
-      btnText : "Click again!"
-    })
+    if (this.state.btnText === "Click me") {
+      this.setState ({btnText : "Click again!"})
+    } else {
+      this.setState ({btnText : "Click me"})
+    }
+  }
+  appearSideDiv(){
+    if (this.state.otherDivClass === "hidden") {
+      this.setState ({otherDivClass : ""})
+    }
+  }
+  dissapearDiv () {
+    this.setState({ otherDivClass : "hidden" })
   }
   render () {
     return (
       <div>
-        <button onClick= {this.changeBtnMessage}>
+        <button onClick = {this.changeBtnMessage}>
           { this.state.btnText }
         </button>
+        <div>
+          <div onMouseOver = {this.appearSideDiv} onMouseOut = { this.dissapearDiv }>
+            <p>Soy un div!</p>
+          </div>
+          <div className = {this.state.otherDivClass}>
+            <p>Y yo otro!</p>
+          </div>
+        </div>
       </div>
     )
   }
 }
 
 {/* 
-Con el fin de comprender en profundidad el concepto, uso y detalles de los eventos en React, se desarrollara una aplicación que nos permita:
+Se desarrollara una aplicación que nos permita:
 
 Tener un boton, que al hacer click cambie el estado interno de esa componente (verificar el estado con las react dev tools)
 Tener un <div> que contenga un texto: al pasar el mouse por arriba deberá agregar un texto que diga “inside” en otro div. Al perder foco, se deberá hacer desaparecer el texto de “inside”
