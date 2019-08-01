0386b8ca7bfb36193e13e015c82a54183f055474
mabenunez <39781996+mabenunez@users.noreply.github.com>
2019-08-01 09:52:30 -0300

    structuring folders by week. adding to-do app

--- 2nd-week/weather-app/src/App.js
+++ 2nd-week/weather-app/src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import Header from './header/Header';
-import Footer from './footer/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      < Header/>
-      <Footer/>
+      
     </div>
   );
 }
